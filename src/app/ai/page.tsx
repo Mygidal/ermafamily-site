866710dd432b199db830a3d3999441cdc1ae1f45
Gidal <team@gidal.app>
2025-05-28 10:24:05 +0300

    Работеща форма на чата

---
+++ src/app/ai/page.tsx
@@ -0,0 +1,9 @@
+import AIAssistant from "../../components/AIAssistant";
+
+export default function AIChatPage() {
+  return (
+    <div className="min-h-screen bg-white px-2 pb-6 pt-[80px]">
+      <AIAssistant />
+    </div>
+  );
+}
